f5105aa262cb52fffef7045b9000bc643e2387d6
dlaserbeam <dino.laserbeam@gmail.com>
2015-09-10 23:37:23 -0600

    More editing.

--- story_one/story_one.cpp
+++ story_one/story_one.cpp
@@ -9,13 +9,13 @@ string getString() {
   return aString;
 }
 
-
-
-int main () {
+void welcome () {
   cout << "\n>Enter your name: ";
   string readerName = getString();
-
   cout << "\n>Hello, " << readerName << ". Welcome.\n";
+}
+
+int firstLeg () { //error with chained ifs and elses if direction is 'right'
   cout << "\n>You are walking down a hallway and reach the end. There are two doors,";
   cout << " one to your left, and one to your right. Which do you choose? ";
 
@@ -33,22 +33,39 @@ int main () {
         cout << "\n>Swords are sharp and dangerous, and you are inexperienced. ";
         cout << "You should have chosen more carefully. You are no match for the warrior. ";
         cout << "You accidentally cut yourself preparing for battle, and the warrior ";
-        cout << "takes advantage of your weakness.\n\n>You are dead. The end.\n\n";
+        cout << "takes advantage of your weakness.\n";
         return -1;
     }
     else if (weapon == "bow and arrow" || weapon == "Bow and Arrow" || weapon == "BOW AND ARROW" ||
     weapon == "Bow and Arrow" || weapon == "bow" || weapon == "BOW" || weapon == "Bow") {
-      cout << "\n>You have chosen wisely. Your victory is swift.\n\n>You win. The end.\n\n";
       return 0;
       }
     else if (weapon == "wit" || weapon == "WIT" || weapon == "Wit" ||
     weapon == "Your Wit" || weapon == "YOUR WIT" || weapon == "your wit" || weapon == "Your wit" ||
     weapon == "my wit" || weapon == "My Wit" || weapon == "My wit" || weapon == "MYWIT") {
-      cout << "\n>That's just stupid. Wit isn't a weapon against a warrior.\n";
-      cout << "\n>You are dead. The end.\n\n";
       return -1;
     }
-    else {cout << "\n>Invalid weapon choice.\n\n>You are dead. The end.\n\n"; return -1;}
+    else return -2;
   }
-  else {cout << "\n>Invalid direction choice.\n\n>You are dead. The end.\n\n"; return -1;}
+  else return -2;
+}
+
+int main () {
+  welcome();
+  int step1 = firstLeg();
+  switch (step1) {
+    case 0: {
+      cout << "\n>You have chosen wisely. Your victory is swift.\n\n>You win. The end.\n\n";
+      break;
+    }
+    case -1: {
+      cout << "\n>That's just stupid. Wit isn't a weapon against a warrior.\n";
+      cout << "\n>You are dead. The end.\n\n";
+    }
+    case -2: {
+      cout << "\n>Invalid weapon choice.\n\n>You are dead. The end.\n\n";
+    }
+  }
+
+  return 0;
 }
