072e902e9e9d5a3693d9c44d27122a5e910ae67f
dlaserbeam <dino.laserbeam@gmail.com>
2015-09-15 11:11:43 -0600

    Story two changes.

--- story_two/story_two.cpp
+++ story_two/story_two.cpp
@@ -4,7 +4,6 @@
 
 using namespace std;
 
-
 string getString () {
   string aString;
   cin.sync();
@@ -12,38 +11,82 @@ string getString () {
   return aString;
 }
 
+void invalidChoice() {
+  cout << "\n>>>Invalid choice. Learn how to follow directions.\n\n";
+  exit(2);
+}
+
 int getLanguage () {
-  cout << "Choose language (English or C++): ";
+  cout << ">Choose language (English or C++): ";
   string language = getString();
   if (language == "English" || language == "ENGLISH" || language == "english" || language == "eng" || language == "ENG" || language == "Eng") {
-    cout << "\nStory in English.\n\n";
+    cout << "\n>Story in English.\n\n";
     return 0;
   }
   else if (language == "C++" || language == "c++" || language == "c" || language == "C") {
-    cout << "\nStory in C++.\n\n";
+    cout << "\n>Story in C++.\n\n";
     return 1;
   }
   else {
-    cout << "\nInvalid choice. Learn how to follow directions.\n\n";
-    exit(2);
+    invalidChoice();
   }
 }
 
-int story (int lang) {
+int firstLeg (int lang) {
   switch (lang) {
-    case 0:
-      cout << "Story blah blah blah.\n\n";
-      break;
-    case 1:
-      cout << "// story here\n\n";
-      break;
+    case 0: //eng
+      cout << ">You enter a bar and dicsover to your horror that it's a:\n";
+      cout << "(enter number)\t<1>karaoke bar\t<2>cowbow bar\n";
+      int bar;
+      cin >> bar;
+      return bar;
+    case 1: //c++
+      cout << ">// story here\n\n>Under development.\n\n";
+      return 3;
   }
+}
+
+int bar1 (int barType) {
+  switch (barType) {
+    case 1: //eng, karaoke
+      cout << "\n>Welcome to the karaoke bar, your worst nightmare. Terrible music and even worse singing.\n";
+      cout << "And fuck. Is that your ex? Yes, it is. Standing up on the makeshift stage, singing that song (you know the one), and staring straight at you.\n";
+      cout << "Yeah. This is bad. The way I see it, you have two choices: turn and run, or join in. You pick.\n";
+      cout << "(enter number)\t<1>run\t<2>sing\n";
+      int runSing;
+      cin >> runSing;
+      return runSing;
+    case 2: //eng, cowboy
+      cout << "\n>Welcome to the cowboy bar.\n\n";
+      return 3;
+    case 3: //c++
+      break;
+    default:
+      invalidChoice();
+  }
+}
+
+int karaokeBarRun() {
+  cout << "\n\n>Can't say I blame you. I probably would have run, too.\n\n";
   return 0;
+}
+
+int karaokeBarSing () {
+  cout << "\n\nOkay, you're brave, or insane. Or probably drunk.\n\n";
 }
 
 int main () {
   int lang = getLanguage();
-  story(lang);
+  int barType = firstLeg(lang);
+  int barChoice = bar1(barType);
+  switch (barChoice) {
+    case 1:
+      karaokeBarRun();
+    case 2:
+      karaokeBarSing();
+    case 3:
+      cout << "Cowboy bar under development.";
+  }
 
   return 0;
 }
